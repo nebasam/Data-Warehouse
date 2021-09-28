40d38c72461e675dccc0e65175c850144e4edfbe
Nebiyu Samuel <neba.samuel17@gmail.com>
2021-09-28 14:15:10 +0300

    edits profile

--- target/run/my_new_project/models/station_metadata.sql
+++ target/run/my_new_project/models/station_metadata.sql
@@ -1,10 +1,9 @@
 
 
-  create  table
-    `tech_Stack`.`station_metadata__dbt_tmp`
+  create  table "tech_stack"."dbt"."station_metadata__dbt_tmp"
   as (
     
 
 SELECT ID, FWY, DIR, District, County, City, State_PM, Abs_PM, Latitude, Longitude, Length, Type, Lanes, Name
 FROM analytics.I80Stations
-  )
+  );
